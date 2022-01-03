24d52a350d845d16dd71475023a5c1d286e23292
t8200044 <t8200044@aueb.gr>
2022-01-03 12:21:41 +0200

    Maven project
    Maven Project of Nereids App

--- Nereids eclipse/src/gr/aueb/dmst/nereids/graphicalInterface/Main.java
+++ Nereids eclipse/src/gr/aueb/dmst/nereids/graphicalInterface/Main.java
@@ -2,9 +2,9 @@ package gr.aueb.dmst.nereids.graphicalInterface;
 
 public class Main {
 
-	public static void main(String[] args) throws InterruptedException {
-		Gameplay gameplay = new Gameplay();
-		//DefeatFrame df = new DefeatFrame();
-	}
+    public static void main(String[] args) throws InterruptedException {
+        Gameplay gameplay = new Gameplay();
+        // DefeatFrame df = new DefeatFrame();
+    }
 
 }
